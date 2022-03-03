b292f89e8c8b171f0eb90163aea0867fd4926184
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-03-03 16:19:43 -0500

    Added comments code

--- Game of Life - Jonah Pickett/Game of Life - Jonah Pickett/Form1.cs
+++ Game of Life - Jonah Pickett/Game of Life - Jonah Pickett/Form1.cs
@@ -41,6 +41,7 @@ namespace Game_of_Life___Jonah_Pickett
         // Calculate the next generation of cells
         private void NextGeneration()
         {
+            // Create scratchPad
             bool[,] scratchPad = new bool[universe.GetLength(0), universe.GetLength(1)];
 
             for ( int y = 0; y < universe.GetLength(1); y++)
@@ -50,29 +51,23 @@ namespace Game_of_Life___Jonah_Pickett
                 {
                     int count = CountNeighbors(x, y);
 
-
-                    // Apply the rules
-                    if (universe[x, y] == true && count < 2)
+                    // Apply the rules & turn on/off in scratchPad
+                    if (universe[x, y] == true && count < 2) // Rule #1
                     {
                         scratchPad[x, y] = false;
                     }
-                    else if (universe[x, y] == true && (count == 2 || count == 3))
+                    else if (universe[x, y] == true && (count == 2 || count == 3)) // Rule #3
                     {
                         scratchPad[x, y] = true;
                     }
-                    else if (universe[x, y] == true && count > 3)
+                    else if (universe[x, y] == true && count > 3) // Rule #2
                     {
                         scratchPad[x, y] = false;
                     }
-                    else if (universe[x, y] == false && count == 3)
+                    else if (universe[x, y] == false && count == 3) // Rule #4
                     {
                         scratchPad[x, y] = true;
                     }
-
-
-                    // Turn it on/off in the scratchPad
-
-
                 }
             }
 
@@ -226,24 +221,32 @@ namespace Game_of_Life___Jonah_Pickett
         // Next button (click event)
         private void toolStripButton3_Click(object sender, EventArgs e)
         {
-            NextGeneration();
+            NextGeneration(); // advance one generation
         }
 
         // File -> New button (click event)
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
-        {                       
+        {        
+            // Generate blank universe
             for (int y = 0; y < universe.GetLength(1); y++)
             {
                 for (int x = 0; x < universe.GetLength(0); x++)
                 {
                     universe[x, y] = false;
                 }
-            }                       
-            timer.Enabled = false;
-            generations = 0;
-            livingCells = 0;
+            }
+            
+            timer.Enabled = false; // stops generation count 
+
+            // resets generation count to 0
+            generations = 0; 
             toolStripStatusLabelGenerations.Text = "Generations = " + generations.ToString();
+
+            // resets living cells count to 0 
+            livingCells = 0;          
             toolStripStatusLabelAlive.Text = "Alive = " + livingCells.ToString();
+
+            // refresh to display update
             graphicsPanel1.Invalidate();
         }       
 
@@ -251,6 +254,8 @@ namespace Game_of_Life___Jonah_Pickett
         private int CountNeighbors(int x, int y)
         {
             int neighbors = 0;
+
+            // Determine whether toroidal/finite is toggled on/off, and applies appropriate CountNeighbors() function
             if(toroidalToolStripMenuItem.Checked == true)
             {
                 neighbors = CountNeighborsToroidal(x, y);
@@ -297,6 +302,7 @@ namespace Game_of_Life___Jonah_Pickett
                         continue;
                     }
 
+                    // check to see if neighboring cell is alive, and add to count
                     if (universe[xCheck, yCheck] == true) count++;
                 }
             }
@@ -337,6 +343,7 @@ namespace Game_of_Life___Jonah_Pickett
                         yCheck = 0;
                     }
 
+                    // check to see if neighboring cell is alive, and add to count
                     if (universe[xCheck, yCheck] == true) count++;
                 }
             }
@@ -353,6 +360,7 @@ namespace Game_of_Life___Jonah_Pickett
         private void toroidalToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
         {
             bool toroidalChecked = toroidalToolStripMenuItem.Checked;
+            // Toggle finite view based on whether toroidal is checked (mutually exclusive)
             if (finiteToolStripMenuItem.Checked == true && toroidalChecked == true)
             {
                 finiteToolStripMenuItem.Checked = false;
@@ -362,17 +370,19 @@ namespace Game_of_Life___Jonah_Pickett
                 finiteToolStripMenuItem.Checked = true;
             }
 
+            // Refresh screen to update
             graphicsPanel1.Invalidate();
         }
         private void toroidalToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            toroidalToolStripMenuItem.Checked = !toroidalToolStripMenuItem.Checked;
+            toroidalToolStripMenuItem.Checked = !toroidalToolStripMenuItem.Checked; // toggle toroidal view
         }
 
         // View -> Finite button
         private void finiteToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
         {
             bool finiteChecked = finiteToolStripMenuItem.Checked;
+            // Toggle toroidal view based on whether finite is checked (mutually exclusive)
             if(finiteChecked == true && toroidalToolStripMenuItem.Checked == true)
             {
                 toroidalToolStripMenuItem.Checked = false;
@@ -381,11 +391,13 @@ namespace Game_of_Life___Jonah_Pickett
             {
                 toroidalToolStripMenuItem.Checked = true;
             }
+
+            // Refresh screen to update
             graphicsPanel1.Invalidate();
         }
         private void finiteToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            finiteToolStripMenuItem.Checked = !finiteToolStripMenuItem.Checked;
+            finiteToolStripMenuItem.Checked = !finiteToolStripMenuItem.Checked; // toggle fintie view
         }
 
         // Initial Randomize Universe
@@ -416,13 +428,16 @@ namespace Game_of_Life___Jonah_Pickett
         }
         // Randomize Universe by Time
         private void RandomizeUniverseByTime()
-        {            
+        {   
+            //Random # Generator - based off current time in ticks
             Random timerng = new Random((int)DateTime.Now.Ticks);
+
+            // Loop through universe
             for (int y = 0; y < universe.GetLength(1); y++)
             {
                 for (int x = 0; x < universe.GetLength(0); x++)
                 {
-
+                    // Generate random # (0-2) to see if that cell will be alive/dead
                     int number = timerng.Next(0, 3);
                     if (number == 0)
                     {
@@ -433,6 +448,7 @@ namespace Game_of_Life___Jonah_Pickett
                 }
             }
 
+            // Refresh screen to update
             graphicsPanel1.Invalidate();
         }
 
@@ -445,8 +461,8 @@ namespace Game_of_Life___Jonah_Pickett
         // View -> Counting Numbers button
         private void neighborCountToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            neighborCountToolStripMenuItem1.Checked = !neighborCountToolStripMenuItem1.Checked;
-            neighborCountToolStripMenuItem.Checked = !neighborCountToolStripMenuItem.Checked;
+            neighborCountToolStripMenuItem1.Checked = !neighborCountToolStripMenuItem1.Checked; // Context menu strip (View -> Neighbor Count button)
+            neighborCountToolStripMenuItem.Checked = !neighborCountToolStripMenuItem.Checked; // toggle neighbor count
             graphicsPanel1.Invalidate();
         }
 
@@ -454,7 +470,7 @@ namespace Game_of_Life___Jonah_Pickett
         private void gridToolStripMenuItem_Click(object sender, EventArgs e)
         {
             gridToolStripMenuItem1.Checked = !gridToolStripMenuItem1.Checked; // context menu strip (View -> Grid button)
-            gridToolStripMenuItem.Checked = !gridToolStripMenuItem.Checked;
+            gridToolStripMenuItem.Checked = !gridToolStripMenuItem.Checked; // toggle grid
             graphicsPanel1.Invalidate();
         }
 
