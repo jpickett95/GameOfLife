d5d6099de704f296168c9d9625d3580085c9be09
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-03-03 16:27:56 -0500

    Added comments to double buffering code;

--- Game of Life - Jonah Pickett/Game of Life - Jonah Pickett/GraphicsPanel.cs
+++ Game of Life - Jonah Pickett/Game of Life - Jonah Pickett/GraphicsPanel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Double Buffering
 // Change the namespace to your project's namespace.
 namespace Game_of_Life___Jonah_Pickett
 {
